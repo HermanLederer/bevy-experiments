eb3066d5b7225901d2f6ba3a9960ebac0c238292
Herman Lederer <germans.lederers@gmail.com>
2022-08-01 15:06:16 +0300

    Disable VSync

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod plugins;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PresentMode};
 use plugins::lesson_2::Lesson2Plugin;
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .insert_resource(WindowDescriptor {
+            present_mode: PresentMode::AutoNoVsync,
+            ..default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(Lesson2Plugin)
         .run();

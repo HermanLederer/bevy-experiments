941e260f7ea6e277ed6662c5701a455b9c755fb9
Herman Lederer <germans.lederers@gmail.com>
2022-08-01 12:57:08 +0300

    Create a plugin template

---
+++ templates/new_plugin.rs
@@ -0,0 +1,23 @@
+use bevy::prelude::*;
+
+//
+//
+// Plugin
+
+pub struct NewPlugin;
+
+impl Plugin for NewPlugin {
+    fn build(&self, app: &mut App) {}
+}
+
+//
+//
+// Components
+
+//
+//
+// Resources
+
+//
+//
+// Systems
